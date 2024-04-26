771039416d6b94919ad198d83022b3e63c10a5ad
leesoul2 <leesoul284@gmail.com>
2024-04-26 17:51:45 +0900

    ddl, dml 수정

--- SQL/esolsemiproject_DML.sql
+++ SQL/esolsemiproject_DML.sql
@@ -4,29 +4,48 @@ INSERT INTO member VALUES('aab','abcd','ab@gmail.com');
 INSERT INTO member VALUES('aac','abcde','a@gmail.com');
 INSERT INTO member VALUES('test','kh','a@gmail.com');
 
-
-INSERT INTO login_log VALUES('2024-04-11','123.456.789.999','aaa','1');
-
 SELECT * FROM board;
 INSERT INTO board VALUES(105,'aaa','제목','내용내용','20240412','20240412','','','','','');
+
+SELECT * FROM board_file;
+INSERT INTO board_file VALUES(1,105,'파일경로명','파일명');
+
+SELECT * FROM COMMENTS;
+INSERT INTO COMMENTS VALUES(1,105,'댓글내용','20240411','aaa');
+INSERT INTO COMMENTS VALUES(2,105,'댓글내용','20240411','aab');
+INSERT INTO COMMENTS VALUES(3,105,'댓글내용','20240411','aac');
+
+SELECT * FROM game;
+INSERT INTO game VALUES(1001, 24000, 'gametitle', 'explanation', 'tag','2024-04-11 00:00:00', '1', '1','1');
+
+SELECT * FROM game_buy;
+INSERT INTO game VALUES();
+
+SELECT * FROM comment_post;
+INSERT INTO game VALUES();
+
+SELECT * FROM gamereview;
+INSERT INTO gamereview VALUES();
+
+INSERT INTO wishlist VALUES();
 
 SELECT * FROM board_post;
 INSERT INTO board_post VALUES();
 
-SELECT * FROM board_file;
-INSERT INTO board_file VALUES();
+INSERT INTO login_log VALUES('2024-04-11','123.456.789.999','aaa','1');
 
 
-SELECT * FROM COMMENTS;
-INSERT INTO COMMENTS VALUES('1','1','댓글내용','20240411','aaa');
-INSERT INTO COMMENTS VALUES('2','1','댓글내용','20240411','aab');
-INSERT INTO COMMENTS VALUES('3','2','댓글내용','20240411','aac');
 
-INSERT INTO wishlist VALUES();
 
-SELECT * FROM game;
-INSERT INTO game VALUES(1001, 24000, 'gametitle', 'explanation', 'tag','2024-04-11 00:00:00', '1', '1');
 
-SELECT * FROM gamereview;
-INSERT INTO gamereview VALUES();
+
+
+
+
+
+
+
+
+
+
 
